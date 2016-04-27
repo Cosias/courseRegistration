8204840e02d26894e42ad592bad8d70aa2f60ce8
Cosias <cosias@lions.molloy.edu>
2016-04-27 15:23:03 -0400

    Made changes to the form

--- data/prefQuestionaire.js
+++ data/prefQuestionaire.js
@@ -1,8 +1,11 @@
- 
-  $(".previousbutton").change(function() {
-    if(this.checked){$("#form_sub_container2").show();}
-    else{
-    $(".form_sub").hide();
+
+
+$(".monday").change(function() {
+    if(this.checked){
+    $(".monday_form").show();
+    } else if(!this.checked){
+    $(".monday_form").hide();
+    $(".monday_form").children().attr('checked',false);
     }})
 
     $("#nextbutton").click(function() {
@@ -10,4 +13,42 @@
   //  $("#form_sub_container1").hide();
 })
 
+$(".tuesday").change(function() {
+    if(this.checked){
+    $(".tuesday_form").show();
+    } else if(!this.checked){
+    $(".tuesday_form").hide();
+    $(".tuesday_form").children().attr('checked',false);
+    }})
 
+$(".wednesday").change(function() {
+    if(this.checked){
+    $(".wednesday_form").show();
+    } else if(!this.checked){
+    $(".wednesday_form").hide();
+    $(".wednesday_form").children().attr('checked',false);
+    }})
+    
+$(".thursday").change(function() {
+    if(this.checked){
+    $(".thursday_form").show();
+    } else if(!this.checked){
+    $(".thursday_form").hide();
+    $(".thursday_form").children().attr('checked',false);
+    }})
+    
+$(".friday").change(function() {
+    if(this.checked){
+    $(".friday_form").show();
+    } else if(!this.checked){
+    $(".friday_form").hide();
+    $(".friday_form").children().attr('checked',false);
+    }})
+    
+$(".saturday").change(function() {
+    if(this.checked){
+    $(".saturday_form").show();
+    } else if(!this.checked){
+    $(".saturday_form").hide();
+    $(".saturday_form").children().attr('checked',false);
+    }})
